4b194ebeb844d571377acc39106b4f4d39370979
Adam Orr <adamo@Adams-MacBook-Pro.local>
2021-03-12 17:50:36 -0700

    New website Frist draft

--- demos/examples/README.md
+++
@@ -1,1 +0,0 @@
-Simple image display with jQuery and JSON
---
+++ script.js
@@ -0,0 +1,38 @@
+// const n = document.getElementById('n');
+
+// const getRandomCharacter = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1)) + min;
+
+// const generate = (limit, min, max) => {
+//   let charactersList = [];
+
+//   do {
+//     charactersList.push(String.fromCharCode(getRandomCharacter(min, max)));
+//   } while (--limit);
+
+//   return charactersList.join('');
+// };
+
+// setInterval(() => {
+//   let initial = n.getAttribute('data-initial');
+//   let len = initial.length;
+
+//   for (let i = 0; i < 100; i++) {
+//     setTimeout(() => {
+//       let gen = generate(len, 48, 57);
+//       n.innerText = gen;
+//       document.title = gen;
+//     }, i * 10);
+//   }
+
+//   setTimeout(() => {
+//     n.innerText = initial;
+//     document.title = initial;
+//   }, 1000);
+// }, 5000);
+
+
+var x = 5;
+for(Var I) {
+	
+}
